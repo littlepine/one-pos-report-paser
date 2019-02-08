8040f2265917ed68c0f38a20871efce20ac376ed
Pine <pine+git@justmove.hk>
2019-02-08 16:19:04 +0800

    Refine the script to remove file name dependance

--- rename.sh
+++ rename.sh
@@ -5,9 +5,17 @@ mkdir -p output
 
 declare -A branch=( ["AA"]="粉嶺" ["AB"]="馬鞍山" ["AC"]="荃灣" ["AD"]="上水" ["AE"]="九龍灣")
 
-for branchCode in "${!branch[@]}";
-do
-    pdfseparate -f 1 input/*${branchCode}.pdf input/${branchCode}_p%d.pdf
+# remove space in file name
+find ./input/ -name "* *" -type f | rename 's/ /_/g'
+
+for file in ./input/*.pdf
+do 
+    echo "Processing file $file"
+    branchCode=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 30,190,50,202 -i $file)
+    branchCode=${branchCode//[$'\t\r\n']}
+    echo "Detected branch code $branchCode"
+    cp $file input/$branchCode.pdf
+    pdfseparate -f 1 input/${branchCode}.pdf input/${branchCode}_p%d.pdf
     for file in ./input/${branchCode}_p*.pdf
     do
         supplierName=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 60,50,80,1000 -i $file)
@@ -18,3 +26,19 @@ do
         cp $file output/$supplierName/$supplierName.${branch[$branchCode]}.pdf
     done
 done
+
+find ./input/ -name "* *" -type f | rename 's/ /_/g'
+
+# for branchCode in "${!branch[@]}";
+# do
+#     pdfseparate -f 1 input/*${branchCode}.pdf input/${branchCode}_p%d.pdf
+#     for file in ./input/${branchCode}_p*.pdf
+#     do
+#         supplierName=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 60,50,80,1000 -i $file)
+#         supplierName=${supplierName//\"/}
+#         supplierName=${supplierName//[$'\t\r\n']}
+#         supplierName=${supplierName// /_}
+#         mkdir -p output/$supplierName
+#         cp $file output/$supplierName/$supplierName.${branch[$branchCode]}.pdf
+#     done
+# done

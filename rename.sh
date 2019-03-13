f2f68a11cd613913f25f20ca99cdf54f06cb0ccc
Pine <pine+git@justmove.hk>
2019-03-13 13:27:40 +0800

    Updated PDF dimension for source file

--- rename.sh
+++ rename.sh
@@ -11,17 +11,18 @@ find ./input/ -name "* *" -type f | rename 's/ /_/g'
 for file in ./input/*.pdf
 do 
     echo "Processing file $file"
-    branchCode=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 30,190,50,202 -i $file)
+    branchCode=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 15,150,35,202 -i $file)
     branchCode=${branchCode//[$'\t\r\n']}
     echo "Detected branch code $branchCode"
     cp $file input/$branchCode.pdf
     pdfseparate -f 1 input/${branchCode}.pdf input/${branchCode}_p%d.pdf
     for file in ./input/${branchCode}_p*.pdf
     do
-        supplierName=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 60,50,80,1000 -i $file)
+        supplierName=$(java -jar tabula-1.0.2-jar-with-dependencies.jar -a 60,50,75,500 -i $file)
         supplierName=${supplierName//\"/}
         supplierName=${supplierName//[$'\t\r\n']}
         supplierName=${supplierName// /_}
+	echo "Captured supplier name: "$supplierName
         mkdir -p output/$supplierName
         cp $file output/$supplierName/$supplierName.${branch[$branchCode]}.pdf
     done
